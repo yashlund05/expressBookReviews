f8c4905bf48ca336c0e416c4561bc1d901e519e7
yashlund05 <yashlund05@gmail.com>
2026-09-24 01:29:22 +0530

    Complete Express Book Review final project

--- final_project/index.js
+++ final_project/index.js
@@ -10,10 +10,26 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  if (!req.session.authorization) {
+    return res.status(401).json({
+      message: "User not authenticated"
+    });
+  }
+
+  const token = req.session.authorization.accessToken;
+
+  jwt.verify(token, "access", (err, user) => {
+    if (err) {
+      return res.status(401).json({
+        message: "User not authenticated"
+      });
+    }
+
+    req.user = user;
+    next();
+  });
 });
- 
 const PORT =5000;
 
 app.use("/customer", customer_routes);
